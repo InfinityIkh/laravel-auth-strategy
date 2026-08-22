de90953ab96a0120c30fbafe3fe6cd88c90929e9
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-23 00:23:11 +0200

    refactor: update logout api route

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@ use App\Http\Resources\UserResources;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/{type}/login', [AuthController::class, 'login'])
+    Route::post('auth/{type}/login', [AuthController::class, 'login'])
          ->whereIn('type', ['spa', 'token']);
 });
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout' ,[AuthController::class ,'logout']);
+    Route::post('auth/{type}/logout' ,[AuthController::class ,'logout']);
     Route::get('profile' ,[AuthController::class ,'user']);
 });
