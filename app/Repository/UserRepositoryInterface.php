7d1cd9a5769466d53e00f8847fe9d6c735534cf2
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-25 13:27:34 +0200

    feat(users): add user repository contract

---
+++ app/Repository/UserRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+interface UserRepositoryInterface
+{
+    public function getAll(): Collection;
+
+    public function create(array $data): User;
+
+    public function update(User $user, array $data): User;
+
+    public function delete(User $user): bool;
+}
